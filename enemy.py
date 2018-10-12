3d28e94e546537a9414f6a17bb36d66ecd142eda
Ayah Alkhatib <ayah.alkhatib90@gmail.com>
2018-10-11 22:27:37 -0500

    changed the form of the enemies group

--- enemy.py
+++ enemy.py
@@ -9,41 +9,48 @@ class Enemy(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
 
-    #def add_enemy():
-
+    # def add_enemy():
+    # add groups of enemies
         self.pandaActor = Actor("models/panda-model",
                                 {"walk": "models/panda-walk4"})
         self.pandaActor.setScale(0.001, 0.001, 0.001)
-        #self.pandaActor.setPos(-9,20,4)
         self.pandaActor.loop("walk")
         self.pandaActor.reparentTo(self.render)
-        for j in range (10):
-            for i in range (10):
+
+        for j in range (4):
+            for i in range (5):
                 self.placeholder = self.render.attachNewNode("panda")
-                self.placeholder.setPos(-9+(i*2),20,4)
+                self.placeholder.setPos(-9,20,4-i)
                 self.pandaActor.instanceTo(self.placeholder)
-            self.placeholder2 = self.render.attachNewNode("panda")
-            self.placeholder2.setPos(-8+(j*2),20,2)
-            self.pandaActor.instanceTo(self.placeholder2)
-            pandaPosInterval1 = self.pandaActor.posInterval(10,
-                                                            Point3(-5, 10, 0),
-                                                            startPos=Point3(0, 10, 0))
-            pandaPosInterval2 = self.pandaActor.posInterval(10,
-                                                            Point3(5, 10, 0),
-                                                            startPos=Point3(0, 10, 0))
-            pandaHprInterval1 = self.pandaActor.hprInterval(3,
-                                                            Point3(180, 0, 0),
-                                                            startHpr=Point3(0, 0, 0))
-            pandaHprInterval2 = self.pandaActor.hprInterval(3,
-                                                            Point3(0, 0, 0),
-                                                            startHpr=Point3(180, 0, 0))
+            #self.placeholder2 = self.render.attachNewNode("panda")
+            self.placeholder.setPos(-8,20,4-j)
+            self.pandaActor.instanceTo(self.placeholder)
+
+        pandaPosInterval1 = self.pandaActor.posInterval(10,
+                                                        Point3(-5, 20, 0),
+                                                        startPos=Point3(-9,20, 4))
+        pandaPosInterval2 = self.pandaActor.posInterval(10,
+                                                        Point3(-9, 20, 0),
+                                                        startPos=Point3(-5, 20, 0))
+        pandaPosInterval3 = self.pandaActor.posInterval(10,
+                                                        Point3(-9, 20, 4),
+                                                        startPos=Point3(-9, 20, 0))
+            #pandaHprInterval1 = self.pandaActor.hprInterval(3,
+            #                                                Point3(180, 0, 0),
+            #                                                startHpr=Point3(0, 0, 0))
+            #pandaHprInterval2 = self.pandaActor.hprInterval(3,
+            #                                                Point3(0, 0, 0),
+            #                                                startHpr=Point3(180, 0, 0))
 
 
             # Create and play the sequence that coordinates the intervals.
-            self.pandaPace = Sequence(pandaPosInterval1,
-                                      pandaHprInterval1,
-                                      pandaPosInterval2,
-                                      pandaHprInterval2,
-                                      name="pandaPace")
-            self.pandaPace.loop()
+        self.pandaPace = Sequence(pandaPosInterval1,
+        #                          pandaHprInterval1,
+                                  pandaPosInterval2,
+        #                          pandaHprInterval2,
+                                  pandaPosInterval3,
+                                  name="pandaPace")
+        self.pandaPace.loop()
+
+
         # Loop its animation.
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from enemy import Enemy 
+from enemy import Enemy
 app = Enemy()
 # run should be called once at the end, I am using it here just for testing purposes.
+
 app.run()
