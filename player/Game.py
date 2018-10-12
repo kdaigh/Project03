928828384645418989bd0905fa5e96a2044adfdd
cmeyer <artgirlie26@gmail.com>
2018-10-12 12:51:11 -0500

    added comments to the game file because that will be edited by everyone

--- player/Game.py
+++ player/Game.py
@@ -8,9 +8,14 @@ screen = pygame.display.set_mode((900, 600))
 # label window
 pygame.display.set_caption("Py Five")
 
+#setting the background to the starts
 bg = pygame.image.load("space.jpg")
 screen.blit(bg, (0,0))
 
+#instantiating the ship, you have to put the current screen and the background
+#in because the ships movements are covered with the background
 playing = Ship(450,410,64,64,screen,bg)
+
 while 1:
+    #allows the user to control the ship that was placed
     playing.controls()
