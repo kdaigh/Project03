3080b021d63880d5c44e30ebb75d4cd584f06fc6
cmeyer <artgirlie26@gmail.com>
2018-10-12 12:05:10 -0500

    got it to move while holding down the left and right keys

--- player/main.py
+++ player/main.py
@@ -8,23 +8,27 @@ class Ship(object):
         self.y = y
         self.width = width
         self.height = height
-        self.moveCount = 0;
+        self.moveCount = 0
+        self.left=False
+        self.right=False
 
         self.ship = pygame.image.load("ship.png")
         screen.blit(self.ship, (450, 410))
         #pygame.display.update()
+    def move(self,left,right):
+        #def walkLeft(self):
+        if left:
+            self.x=self.x-10
+            screen.blit(bg, (0,0))
+            screen.blit(self.ship, (self.x, self.y))
+            #pygame.display.update()
 
-    def walkLeft(self):
-        self.x=self.x-10
-        screen.blit(bg, (0,0))
-        screen.blit(self.ship, (self.x, self.y))
-        #pygame.display.update()
-
-    def walkRight(self):
-        self.x=self.x+10
-        screen.blit(bg, (0,0))
-        screen.blit(self.ship, (self.x, self.y))
-        #pygame.display.update()
+        #def walkRight(self):
+        if right:
+            self.x=self.x+10
+            screen.blit(bg, (0,0))
+            screen.blit(self.ship, (self.x, self.y))
+            #pygame.display.update()
 
 # setting window size
 screen = pygame.display.set_mode((900, 600))
@@ -37,12 +41,27 @@ screen.blit(bg, (0,0))
 #pygame.display.update()
 #screen.fill(0,0,255)
 playing = Ship(450,410,64,64)
+left=False
+right=False
 while 1:
+    playing.move(left,right)
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playing.walkLeft()
-            elif event.key == pygame.K_RIGHT:
-                playing.walkRight()
+                left=True
+            if event.key == pygame.K_RIGHT:
+                right=True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                left=False
+            if event.key ==pygame.K_RIGHT:
+                right=False
+
+
+        #if event.type == pygame.KEYDOWN:
+        #    if event.key == pygame.K_LEFT:
+        #        playing.walkLeft()
+        #    elif event.key == pygame.K_RIGHT:
+        #        playing.walkRight()
         if event.type == pygame.QUIT: sys.exit()
     pygame.display.update()
