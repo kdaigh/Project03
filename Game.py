79f22a5f1b36bb1fc433674e78b5a9de634842ed
Kristi Daigh <kdaigh20@gmail.com>
2018-10-17 13:39:50 -0500

    game.py: Refactored game class

--- Game.py
+++ Game.py
@@ -1,32 +1,39 @@
+## @file game.py
+#  Source file for game class
+#
+#  Project: Gallaga Clone
+#  Author: Py Five
+#  Created: 10/14/19
+
 import pygame
-from pygame.locals import *
-pygame.init()
-from player.Ship import Ship
-from Enemy.Enemy import Enemy
-import threading
-from threading import Thread
 
-# setting window size
-screen = pygame.display.set_mode((900, 600))
-# label window
-pygame.display.set_caption("Py Five")
 
-#setting the background to the starts
-bg = pygame.image.load('images/space.jpg')
-screen.blit(bg, (0,0))
+class Game:
 
-#instantiating the ship, you have to put the current screen and the background
-#in because the ships movements are covered with the background
-playing = Ship(418,410,64,64,screen,bg)
-enemy = Enemy(400,400,52,48,screen,bg)
+    ## Constructor
+    def __init__(self):
+        pass
 
-while 1:
-    #allows the user to control the ship that was place
-    #Thread(target=enemy.add_enemies()).start()
-    #playing.controls()
-    enemy.add_enemies()
+    ## Sets up the game
+    def setup(self):
 
-    #added functionality to shot
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_SPACE:
-            Shot.append(Shot(player))
+        # setting window size
+        screen = pygame.display.set_mode((900, 600))
+
+        # label window
+        pygame.display.set_caption("Py Five")
+
+        #setting the background to the starts
+        bg = pygame.image.load('images/space.jpg')
+        screen.blit(bg, (0, 0))
+
+    ## Runs the game session
+    def run(self):
+
+        pygame.init()
+
+        while 1:
+            #added functionality to shot
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    Shot.append(Shot(player))
