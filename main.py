b89adf59c16d8733adfc5be0d644015bdae3845a
Kristi Daigh <kdaigh20@gmail.com>
2018-10-17 13:11:14 -0500

    Created main.py file

---
+++ main.py
@@ -0,0 +1,12 @@
+## @file main.py
+#  Main file for project
+#
+#  Project: Gallaga Clone
+#  Author: Py Five
+#  Created: 10/17/19
+
+# TO DO: import statements
+
+# TO DO: game instance
+
+# TO DO: call game run function
