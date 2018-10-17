0628ec336544335e27b378912a0c9f62ee64b4f2
Kristi Daigh <kdaigh20@gmail.com>
2018-10-17 16:29:44 -0500

    player.py: Updates to constructor. Added outline for move() function.

--- player.py
+++ player.py
@@ -14,4 +14,13 @@ class Player(Actor):
 
     ## Constructor
     def __init__(self):
+        # TO DO: call to Actor constructor
+        self.alive = True
+        self.reloading = False
+        # TO DO: set initial coordinates
+
+    ## TO DO
+    ## Moves player in a specific direction
+    #  @param: direction, coordinates that represent desired move
+    def move(self, direction):
         pass
