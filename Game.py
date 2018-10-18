3990dacc6e7e3183557bba01da50e9701d3d5e14
Kristi Daigh <kdaigh20@gmail.com>
2018-10-18 09:33:27 -0500

    game.py, globals.py: Added globals file and improved functionality in game.py

--- Game.py
+++ Game.py
@@ -6,33 +6,43 @@
 #  Created: 10/14/19
 
 import pygame
+from globals import *
 
 
 class Game:
 
     ## Constructor
     def __init__(self):
-        pass
+        self.setup()
 
     ## Sets up the game
     def setup(self):
 
-        # setting window size
-        screen = pygame.display.set_mode((900, 600))
+        # Initialize Game Peripherals
+        pygame.init()
+        screen = pygame.display.set_mode(width, height)
+        clock = pygame.time.Clock()
 
-        # label window
-        pygame.display.set_caption("Py Five")
+        # TO DO: Load Images
 
-        #setting the background to the starts
+
+        # TO DO: Load Background
         bg = pygame.image.load('images/space.jpg')
         screen.blit(bg, (0, 0))
+
+        # Setup Game Window
+        icon = pygame.transform.scale(Alien.images[0], (32, 32))
+        pygame.display.set_icon(icon)
+        pygame.display.set_caption('Gallaga Clone')
+        pygame.mouse.set_visible(0)
+
+        # TO DO: Initialize Starting Actors
 
     ## Runs the game session
     def run(self):
 
-        pygame.init()
+        while player.alive:
 
-        while 1:
             #added functionality to shot
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
--- actor.py
+++ actor.py
@@ -7,6 +7,7 @@
 
 import pygame
 import os.path
+import constants as const
 
 
 ## @class Actor
