5c3e7055bc021a81a361112eb3681bf5dfda2197
Ayah Alkhatib <ayah.alkhatib90@gmail.com>
2018-10-19 09:15:26 -0500

    adding some stuff to enemy.py

--- enemy.py
+++ enemy.py
@@ -6,7 +6,8 @@
 #  Created: 10/17/19
 
 from actor import Actor
-
+from globals import *
+import constants
 
 ## @class Enemy
 #  @brief Implements Actor base class as Enemy object
@@ -16,6 +17,9 @@ class Enemy(Actor):
     ## Constructor
     def __init__(self, image):
         Actor.__init__(self, image)
+        self.hit = False
+        # For now we are not letting
+
 
 
     ## TO DO
