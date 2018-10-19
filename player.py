0ec679edf7f5b4b96bf40137c02983c23c4f1111
Kyle Curry <kylecurry1990@gmail.com>
2018-10-18 22:22:23 -0500

    updating player movement

--- player.py
+++ player.py
@@ -7,6 +7,7 @@
 
 from actor import Actor
 from globals import *
+import constants
 
 
 ## @class Player
@@ -25,4 +26,4 @@ class Player(Actor):
     ## Moves player in a specific direction
     #  @param: direction, coordinates that represent desired move
     def move(self, direction):
-        pass
+        self.rect = self.rect.move(direction * constants.PLAYER_SPEED, 0).clamp(screen)
